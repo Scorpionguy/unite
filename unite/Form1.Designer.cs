d850d133bff21d4ccf83bc5ceb7d816431df4de0
Scorpionguy <lapinfedor11@gmail.com>
2024-02-21 14:57:35 +0300

    Добавьте файлы проекта.

---
+++ unite/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace unite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(244, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1154, 404);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сотрудники";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Экспонаты";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выставки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 67);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Объекты";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(244, 422);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 68);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Кнопка";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 568);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ unite/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace unite
+{
+    public partial class Form1 : Form
+    {
+        public string connectionStr = "server=localhost;username=postgres;database=museum;port=5432;password=1234";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.Size = new System.Drawing.Size(Width - 210, Height - 155);
+            button5.Location = new System.Drawing.Point(dataGridView1.Location.X, dataGridView1.Location.Y + dataGridView1.Height + 5);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            dataGridView1.Size = new System.Drawing.Size(Width - 210, Height - 155);
+            button5.Location = new System.Drawing.Point(dataGridView1.Location.X, dataGridView1.Location.Y + dataGridView1.Height + 5);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStr))
+            {
+                string query1 = "select t.name as Предмет, s.name as Название, s.date_start as Дата_Начала, s.date_end as Дата_Окончания, s.museum as Музей from things t\r\njoin ts on thing_id = fk_thing_id\r\njoin shows s on fk_show_id = show_id";
+                NpgsqlCommand select = new NpgsqlCommand(query1, connection);
+                connection.Open();
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(select);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                connection.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStr))
+            {
+                string query1 = "select fio as ФИО, job_title as Профессия, t.name as Название, t.description as Описание_Объекта, t.restoration as Тип_Реставрации, t.date_restoration as Дата_Реставрации, t.status as Статус, t.number_hall as Номер_Зала from employees\r\njoin et on employee_id = fk_employee_id\r\njoin things t on thing_id = fk_thing_id";
+                NpgsqlCommand select = new NpgsqlCommand(query1, connection);
+                connection.Open();
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(select);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                connection.Close();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStr))
+            {
+                string query1 = "select p.name as Название_Журнала, p.category as Категория, p.date_published as Дата_Публикации, p.isbn as ИСБН,  t.name as Объект, t.description as Описание from publications p\r\njoin tp on publication_id = fk_publication_id\r\njoin things t on thing_id = fk_thing_id";
+                NpgsqlCommand select = new NpgsqlCommand(query1, connection);
+                connection.Open();
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(select);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                connection.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionStr))
+            {
+                string query1 = "select name as Название, description as Описание, date_created as Дата_Создания, date_of_entry as Дата_Поступления, category as Категория, fio as ФИО, restoration as Тип_Реставрации, date_restoration as Дата_Реставрации, status as Статус, number_hall as Номер_Зала from things\r\njoin et on thing_id = fk_thing_id\r\njoin employees on employee_id = fk_employee_id";
+                NpgsqlCommand select = new NpgsqlCommand(query1, connection);
+                connection.Open();
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(select);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                connection.Close();
+            }
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+    }
+}
